57dfa5c580ef8b95d5690b89b5fe4e4f9b470090
Andrew K <andrewkats91@gmail.com>
2019-10-25 12:33:53 +1100

    Initial build test

---
+++ src/main/java/com/andrewkats/katsprint/App.java
@@ -0,0 +1,9 @@
+package com.andrewkats.katsprint;
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/andrewkats/katsprint/AppTest.java
@@ -0,0 +1,14 @@
+package com.andrewkats.katsprint;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void testBasic()
+    {
+        assertTrue( true );
+    }
+}
