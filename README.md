7153660038d050acf5f1e1e73ea515f2322e0fe0
Andrew K <andrewkats91@gmail.com>
2019-10-25 14:32:21 +1100

    Added some useful information and updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # kats-print
-
 A simple console application to calculate predefined print costs.
+
+## Project Layout
+Core src files located at: ```src/main/java/com/andrewkats/katsprint```
+The project is split into three key sections.
+### calc
+Contains methods and tools for calculations and processing.
+### data
+Contains data types and object structure related information.
+Change internal pricing and other data related aspects here.
+### parsing
+Currently not implemented.
 
 ## Technologies
 Ensure you have install the required technologies and they are functioning correctly.
@@ -23,6 +33,5 @@ mvn test
 ```
 
 ## License
-
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
 
--- src/main/java/com/andrewkats/katsprint/data/Paper.java
+++ src/main/java/com/andrewkats/katsprint/data/Paper.java
@@ -4,6 +4,35 @@ public final class Paper
 {
     public final static int PRICE_INVALID = -1;
 
+    // Change internal pricing here.
+    // Use PRICE_INVALID to set a price as unavaliable.
+    public static enum Price
+    {
+        A4_BLACK (15, 10),
+        A4_COLOR (25, 20);
+
+        // Price Internal
+        private final int priceSingle;
+        private final int priceDouble;
+    
+        private Price(int priceSingle, int priceDouble) 
+        {
+            this.priceSingle = priceSingle;
+            this.priceDouble = priceDouble;
+        }
+        
+        public int getPrice(Sided sided) 
+        {
+            if(sided == null) return PRICE_INVALID;
+
+            switch(sided)
+            {
+                case SINGLE: return priceSingle;
+                case DOUBLE: return priceDouble;
+                default: return PRICE_INVALID;
+            }
+        }
+    };
 
     // Support for other paper sizes.
     public static enum Size
@@ -25,33 +54,5 @@ public final class Paper
     {
         BLACKWHITE,
         COLOR
-    };
-
-    // Use PRICE_INVALID to set as unavaliable.
-    public static enum Price
-    {
-        A4_BLACK (15, 10),
-        A4_COLOR (25, 20);
-
-        private final int priceSingle;
-        private final int priceDouble;
-    
-        private Price(int priceSingle, int priceDouble) 
-        {
-            this.priceSingle = priceSingle;
-            this.priceDouble = priceDouble;
-        }
-        
-        public int getPrice(Sided sided) 
-        {
-            if(sided == null) return PRICE_INVALID;
-
-            switch(sided)
-            {
-                case SINGLE: return priceSingle;
-                case DOUBLE: return priceDouble;
-                default: return PRICE_INVALID;
-            }
-        }
     };
 }
